97a425f28010ab53a5ae836bea190c6aaf4b6d69
nekotakkyuu <uffu.lm.tunagi@gmail.com>
2023-11-13 11:24:57 +0900

    kadai_014

---
+++ kadai_014/CarExec_Chapter14.java
@@ -0,0 +1,14 @@
+package kadai_014;
+
+public class CarExec_Chapter14 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		Car_Chapter14 car = new Car_Chapter14(1, 10);
+		
+		car.gearChange(3);
+		car.run();
+
+	}
+}
